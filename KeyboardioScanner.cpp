7d6dfa185199dc5c7ddc4c6dcbf2c72fb12f5da7
matt venn <matt@mattvenn.net>
2018-09-21 14:14:40 +0200

    removed I2C_ERROR bail on write after started getting -1 returns on everything

--- KeyboardioScanner.cpp
+++ KeyboardioScanner.cpp
@@ -158,8 +158,8 @@ int KeyboardioScanner::readRegister(uint8_t cmd) {
 
   uint8_t data[] = {cmd};
   uint8_t result = twi_writeTo(addr, data, ELEMENTS(data), 1, 1); // needed to change stopFlag to 1 to get responses from the tiny
-  if(result == I2C_ERROR)
-    return -1;
+//  if(result == I2C_ERROR)
+ //   return -1;
 
   // needs to be long enough for the slave to respond
   delayMicroseconds(100); 
